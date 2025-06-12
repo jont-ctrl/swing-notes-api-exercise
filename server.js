8a9472efe6985b41c72d371b21fe4b72d09c67b5
Jonatan <180638563+jont-ctrl@users.noreply.github.com>
2025-06-12 11:44:48 +0200

    npm i packages

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware json
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+  res.json({ message: 'Hello world' });
 });
 
 app.listen(PORT, () => {
